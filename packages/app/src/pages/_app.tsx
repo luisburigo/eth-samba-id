e00cc65c46ebd745cf556821863558705286e0ab
Pedro Pereira <pedro.2108@hotmail.com>
2024-03-23 20:55:02 -0300

    fix: wallet config changed to use next js pattern

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,29 +1,37 @@
 import type { AppProps } from 'next/app';
-import { Center, ChakraProvider, ColorModeScript, Container } from '@chakra-ui/react';
+import {
+  Center,
+  ChakraProvider,
+  ColorModeScript,
+  Container,
+} from '@chakra-ui/react';
 import { Header } from '../components/Header';
 import '../theme/global.css';
 import { defaultTheme } from '@/theme/default';
+import Web3ModalProvider from '@/providers/walletConnector/connectProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={defaultTheme}>
-      <ColorModeScript initialColorMode='dark' />
-      <Container
-        w="full"
-        maxW="full"
-        h="100vh"
-        bgColor="background.500"
-        display="flex"
-        flexDir="column"
-        padding={0}
-        overflow="hidden"
-      >
-        <Header.Container />
-        <Center position="relative" w="full" h="full" overflowX="hidden">
-          <Component {...pageProps} />
-        </Center>
-        {/*<TanStackRouterDevtools />*/}
-      </Container>
-    </ChakraProvider>
-  )
+    <Web3ModalProvider>
+      <ChakraProvider theme={defaultTheme}>
+        <ColorModeScript initialColorMode="dark" />
+        <Container
+          w="full"
+          maxW="full"
+          h="100vh"
+          bgColor="background.500"
+          display="flex"
+          flexDir="column"
+          padding={0}
+          overflow="hidden"
+        >
+          <Header.Container />
+          <Center position="relative" w="full" h="full" overflowX="hidden">
+            <Component {...pageProps} />
+          </Center>
+          {/*<TanStackRouterDevtools />*/}
+        </Container>
+      </ChakraProvider>
+    </Web3ModalProvider>
+  );
 }
