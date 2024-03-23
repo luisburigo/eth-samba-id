eb8bf237330ac2d94076543868b361ac30d5084e
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-23 16:45:23 -0300

    feat: added chakra ui

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { defaultTheme } from './theme/default.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={defaultTheme}>
+      <ColorModeScript initialColorMode='dark' />
+      <p>Hi</p>
+    </ChakraProvider>
   </React.StrictMode>,
 )
