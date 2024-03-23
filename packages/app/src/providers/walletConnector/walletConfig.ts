0a828c18cf6c65232339d0f2385a90ca5b0d03b4
Pedro Pereira <pedro.2108@hotmail.com>
2024-03-23 19:28:12 -0300

    feat: scrollSepolia chain added

--- packages/app/src/providers/walletConnector/walletConfig.ts
+++ packages/app/src/providers/walletConnector/walletConfig.ts
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 
 import { cookieStorage, createStorage } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains';
+import { mainnet, scrollSepolia, sepolia } from 'wagmi/chains';
 
 export const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
@@ -14,7 +14,7 @@ const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
-const chains = [mainnet, sepolia] as const;
+const chains = [mainnet, sepolia, scrollSepolia] as const;
 export const config = defaultWagmiConfig({
   chains,
   projectId,
